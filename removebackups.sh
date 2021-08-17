8c8f571264fcb3971ffe781f3d54b53f39d5f853
André Luis <andre@appratico.com>
2021-08-17 09:44:37 -0300

    Added remove files

--- removebackups.sh
+++ removebackups.sh
@@ -6,9 +6,17 @@ path=$(echo "$0" | sed -e "s/removebackups.sh//g")
 # Going to correct path
 cd $path
 
+# Removing files of 'dumpfiles' folder...
+echo "Removing files of 'dumpfiles' folder..."
+rm -rf dumpfiles/*.*
+
 # Removing backups of 'dumpfiles' folder...
 echo "Removing backups of 'dumpfiles' folder..."
 rm -rf dumpfiles/backups/*
+
+# Removing files of 'bqfiles' folder...
+echo "Removing files of 'bqfiles' folder..."
+rm -rf bqfiles/*.*
 
 # Removing backups of 'bqfiles' folder...
 echo "Removing backups of 'bqfiles' folder..."
